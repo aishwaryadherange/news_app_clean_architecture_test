5e3d47c682b2f8893baaa1b3736fc9a867d18cf9
aishwaryadherange <aishwaryadherange9@gmail.com>
2024-03-15 19:20:12 +0530

    Initial commit

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+const String newsAPIBaseURL = 'https://newsapi.org/v2';
+const String newsAPIKey = 'bea0071deed54b8490b094fa3bb538ac';
+const String countryQuery = 'us';
+const String categoryQuery = 'general';
+const String kDefaultImage = "https://img.freepik.com/premium-photo/robot-dog-ai-intelligent-robot-wallpaper-background-illustration-electronic-pet-new-technology_327903-1912201.jpg?w=1800";
